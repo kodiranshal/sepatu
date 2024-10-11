ab7093d80b869371d94bb0a5eef162659cd8be76
irvanfebri <padcyber@gmail.com>
2024-10-11 13:49:50 +0700

    komit pertama

---
+++ app/Services/FrontService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Contracts\CategoryRepositoryInterface;
+use App\Repositories\Contracts\ShoeRepositoryInterface;
+
+class FrontService
+{
+    //dependci injection
+    protected $categoryRepository;
+    protected $shoeRepository;
+
+
+    public function __construct(
+        ShoeRepositoryInterface $shoeRepository,
+        CategoryRepositoryInterface $categoryRepository
+    ) {
+        $this->categoryRepository = $categoryRepository;
+        $this->shoeRepository = $shoeRepository;
+    }
+
+    public function searchShoes(string $keyword)
+    {
+        return $this->shoeRepository->searchByName($keyword);
+    }
+
+    public function getFrontPageData()
+    {
+        $categories = $this->categoryRepository->getAllCategories();
+        $popularShoes = $this->shoeRepository->getPopularShoes(4);
+        $newShoes = $this->shoeRepository->getAllNewShoes();
+
+
+        return compact('categories', 'popularShoes', 'newShoes');
+    }
+}
